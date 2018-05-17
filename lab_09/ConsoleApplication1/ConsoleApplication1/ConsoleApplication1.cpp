1c1219c6950ca6254d4cb128337f574f23a21b48
ZakolM <zakolesnik.m@gmail.com>
2018-05-17 18:32:45 +0300

    lab_09

---
+++ lab_09/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+extern "C" void START();
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	START();
+	return 0;
+}
+
